0f1a3d61adf0e5d2d1ef0cd24ab88bba8f163816
Lou1108 <72265570+Lou1108@users.noreply.github.com>
2022-11-13 22:41:37 +0800

    Prophet univariate and multivariate fixed

--- 3_timeSeriesAnalysis.py
+++ 3_timeSeriesAnalysis.py
@@ -7,46 +7,57 @@ from statsmodels.tsa.statespace.sarimax import SARIMAX
 import matplotlib.pyplot as plt
 
 
-def get_prophet_univariate(train_data, test_data):
+def split_data(d, split_p):
+    # split data into train and test sets
+    split_size = round(len(d) * split_p)
+    train_data = d[:split_size]
+    test_data = d[(split_size - 1):]
+    return train_data, test_data
+
+
+def get_prophet_univariate(d, split_p):
+    train, test = split_data(d, split_p)
     # uni-variate model
     model = Prophet()  # initiating prophet using the default parameters
-    model.fit(train_data)
-    future_model = model.make_future_dataframe(periods=1)
+    model.fit(train)
+    future_model = model.make_future_dataframe(periods=10)
     model_forecast = model.predict(future_model)
 
     plot_prophet(model, model_forecast)
-
-    eval_univariate = pd.merge(test_data, model_forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']][-1:], on='ds')
+    eval_univariate = pd.merge(test, model_forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']], on='ds')
 
     univar_mae = mean_absolute_error(eval_univariate['y'], eval_univariate['yhat'])
     print(f'The univariate MAE is : {univar_mae}')
 
 
 # https://medium.com/grabngoinfo/multivariate-time-series-forecasting-with-seasonality-and-holiday-effect-using-prophet-in-python-d5d4150eeb57
-def get_prophet_multivariate(train_data, test_data, param):
-    print(train_data)
+def get_prophet_multivariate(d, split, param, periods):
+    train, test = split_data(d, split)
     model_multivar = Prophet()  # initiating prophet using the default parameters
     for p in param:
         model_multivar.add_regressor(p)
 
-    model_multivar.fit(train_data)
+    model_multivar.fit(train)
+    future_multivar = create_future_df(model_multivar, param, data, periods)
 
-    # future_multivar = create_future_df(model_multivar, param)
-    multivar_fc = model_multivar.predict(test_data)
+    multivar_fc = model_multivar.predict(future_multivar)  # predicting values
 
     plot_prophet(model_multivar, multivar_fc)
 
     # get performance measure
-    eval_multivar = pd.merge(test_data, multivar_fc, on='ds', how='inner')
+    eval_multivar = pd.merge(test, multivar_fc, on='ds', how='inner')
     multivar_mae = mean_absolute_error(eval_multivar['y'], eval_multivar['yhat'])
     print(f'The univariate MAE is : {multivar_mae}')
 
 
-def create_future_df(model, param):
-    future_multivar = model.make_future_dataframe(periods=10)
+def create_future_df(model, param, d, periods):
+    # create new data set with time range for the forecast
+    future_multivar = model.make_future_dataframe(periods)
 
-    for p in param:
-        future_multivar.insert(1, p, "")
+    # appending new regressor values to df
+    input_df = param.copy()
+    input_df.insert(0, 'ds')
+    future_multivar = pd.merge(future_multivar, d[input_df], on='ds', how='outer')
 
     return future_multivar.fillna(method='ffill')  # Fills missing values with last occurrence
 
@@ -58,12 +69,13 @@ def plot_prophet(model, model_fc):
 
 
 # https://towardsdatascience.com/end-to-end-time-series-analysis-and-forecasting-a-trio-of-sarimax-lstm-and-prophet-part-1-306367e57db8
-def get_sarimax_model(train, test, param):
+def get_sarimax_model(d, split, param):
+    train, test = split_data(d, split)
     train_data = np.array(train['y'], dtype=float)
     train_data_exo = np.array(train[param], dtype=float)
     sarimax_model_fit = sarimax_best_model(train_data, train_data_exo)
 
-    test['fc'] = sarimax_model_fit.predict(start=0, end=len(test.index)-1, exog=test[['ds', param[0]]], dynamic=True)
+    test['fc'] = sarimax_model_fit.predict(start=0, end=len(test.index) - 1, exog=test[['ds', param[0]]], dynamic=True)
     plot_sarimax(train, test)
 
 
@@ -88,7 +100,8 @@ def sarimax_best_model(train_data, train_data_exo):
             continue
 
     # first entry in list has the lowest AIC value, is the assumed to be the best model
-    best_model_df = pd.DataFrame(model_list, columns=['fitted_model', 'aic', 'param']).sort_values(by=['aic'], ascending=True)
+    best_model_df = pd.DataFrame(model_list, columns=['fitted_model', 'aic', 'param']).sort_values(by=['aic'],
+                                                                                                   ascending=True)
     best_model_param = best_model_df.iloc[0].param
     best_model = SARIMAX(train_data, exog=train_data_exo, order=best_model_param[0], seasonal_order=best_model_param[1])
     return best_model.fit(disp=False)
@@ -109,43 +122,19 @@ def plot_sarimax(train, test):
     plt.show()
 
 
-path_index = 'data/index_data.csv'
-data_index = pd.read_csv(path_index, header=0, sep=";")
+path = 'data/preprocessed_and_merged_datasets.csv'
+data = pd.read_csv(path, header=0, sep=";")
 # adjust data for use in prophet
-data_index.columns = ['ds', 'Index Name', 'y']
-data_index['ds'] = pd.to_datetime(data_index['ds'])
-
-hs_index_data = []
-for x in data_index['y']:
-    hs_index_data.append(float(x.replace(',', '')))
-
-data_index['y'] = hs_index_data
-
-# split data into train and test sets
-split_size = round(len(data_index) * 0.8)
-train_index = data_index[:split_size]
-test_index = data_index[split_size:]
-
-path_exo = 'data/weather_data.csv'
-exo_data = pd.read_csv(path_exo, header=0, sep=";") #, parse_dates=['time'], date_parser=date_time_parser)
-exo_data.columns = ['ds', 'condition', 'Wind in km/h', 'Humidity in %', 'Precipitation in mm', 'Visibility in km', 'UV index']
-exo_data['ds'] = pd.to_datetime(exo_data['ds'])
-
-# split data into train and test sets
-split_size = round(len(exo_data) * 0.8)
-train_exo = exo_data[:split_size]
-test_exo = exo_data[split_size:]
+data.rename(columns={'datetime': 'ds'}, inplace=True)
+data.rename(columns={'Index Value': 'y'}, inplace=True)
+data['ds'] = pd.to_datetime(data['ds'])
 
 # Prophet model
-get_prophet_univariate(train_index, test_index)
+get_prophet_univariate(data, 0.75)
 
-multivar_data_train = pd.merge(train_index[['ds', 'y']], train_exo[['ds', 'condition', 'Wind in km/h', 'Humidity in %']], on='ds', how='outer')
-multivar_data_train.index = pd.DatetimeIndex(multivar_data_train['ds'], name='index')
-
-multivar_data_test = pd.merge(test_index[['ds', 'y']], test_exo[['ds', 'condition', 'Wind in km/h', 'Humidity in %']], on='ds', how='outer')
-multivar_data_test.index = pd.DatetimeIndex(multivar_data_test['ds'], name='index')
-
-get_prophet_multivariate(multivar_data_train, multivar_data_test, ['Wind in km/h', 'Humidity in %'])
+get_prophet_multivariate(data, 0.75, ['Wind in km/h', 'ratio_negative_articles'], 16)
 
 # SARIMAX model
-get_sarimax_model(multivar_data_train, multivar_data_test, ['Wind in km/h'])
+# todo: problem is "too few few observations to estimate starting parameters for ARMA and trend. All parameters except for variances will be set to zeros.
+#   warn('Too few observations to estimate starting parameters%s."
+# get_sarimax_model(data, 0.75, ['Wind in km/h'])

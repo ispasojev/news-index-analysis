9168f0d97a16b1522eb176686e2c992314217e04
dario-x <dariobogenreiter@rocketmail.com>
2022-11-15 20:00:25 +0800

    Updating Multiprocessing analysis

--- NLP_Multiprocessing_For_Preprocessing_Experiment.R
+++ NLP_Multiprocessing_For_Preprocessing_Experiment.R
@@ -1,19 +1,28 @@
 
 #load necessary libraries
 library(csv)
+library(future.apply)
+library(ggplot2)
 
-library(microbenchmark)
+print("Number of logical cores:")
+print(detectCores(logical = TRUE))
+print("Number of physical cores:")
+print(detectCores(logical = FALSE))
+
 
 #read in data
-data = read.csv("./Data/news_data.csv", sep = ";")
+all_data = read.csv("./Data/news_data_Experimentation.csv", sep = ";")
 #read in stopwords
-stopwords = read.delim("./Data/stopwords.txt", sep = "\n", skip = 3)
-stopwords = stopwords[,1]
-stopwords = as.vector(stopwords)
+all_stopwords = read.delim("./Data/stopwords.txt", sep = "\n", skip = 3)
+all_stopwords = all_stopwords[,1]
+all_stopwords = as.vector(all_stopwords)
+stopwords = all_stopwords
 
 
 
 remove_stop_words = function(text) {
+  # A simple function that first splits text via
+  # whitespaces and remove then stopwords that appear in list
 
   # split text by whitespace
   x = unlist(strsplit(text, " "))
@@ -21,7 +30,9 @@ remove_stop_words = function(text) {
   x = x[!x %in% stopwords]
   new_text = paste(x, collapse = " ")
   
-  if (nchar(text) > 5) {
+  # if text is longer than 5 characters return modified text
+  # else return original text
+  if (nchar(new_text) > 5) {
     return(new_text)
   }else{
     return(text)
@@ -31,28 +42,66 @@ remove_stop_words = function(text) {
 measure_time = function(code_chunk) {
   
   start_time = Sys.time()
-  eval(code_chunk)
-  Sys.sleep(1)
+  eval(parse(text=code_chunk))
   end_time = Sys.time()
   
   execution_time = end_time - start_time 
-  print(execution_time - 1)
+  execution_time = as.numeric(execution_time, units="secs")
+  print(execution_time)
 }
 
+number_of_stopwords_to_try =  c(100, 2070)
+number_of_articles_to_try =  c(200, 10000, 500000)
+
+for(n_texts in number_of_articles_to_try) {
+  
+  # select desired data
+  data = all_data[1:n_texts,]
+  
+  for (n_stopwords in number_of_stopwords_to_try) {
+    
+    stopwords = all_stopwords[1:n_stopwords]
+  
+    model_name_list = c()
+    execution_time_list = c()
+    
+    model_name_list = append(model_name_list, "Sequential")
+    execution_time_list = append(execution_time_list, 
+                                 measure_time("lapply(data$text, remove_stop_words)"))
+    
+    for (n_cores in c(2,4,8)) {
+      plan(multiprocess, workers = n_workers)
+      model_name_list = append(model_name_list, paste("Future ", n_cores, " Cores"))
+      execution_time_list = append(execution_time_list, 
+                                   measure_time("future_lapply(data$text, remove_stop_words)"))
+    }
+    
+    visuals_df = data.frame(Model = model_name_list, 
+                            Time = execution_time_list)
+    
+    #create a plot of the results
+    results_plot = ggplot(visuals_df, aes(x = Model, y= Time))+
+      geom_bar(stat="identity") +
+      labs(title="Comparison of Multiprocessing Strategies in R", 
+           subtitle = paste("Number of texts:", n_texts,
+                           "Number of Stopwords: ", n_stopwords)) + 
+      xlab("Multiprocessing Approach") +
+      geom_hline(yintercept = min(execution_time_list), color ="red") +
+      ylab("Execution Time in Seconds") + 
+      theme_light()
+    
+    ggsave(paste("./data/images/Multiprocessing_Experiment_R_",
+             n_texts, "_articles_",
+             n_stopwords, "_stopwords.png"), 
+           plot = results_plot)
+    
+  }
+
+}
 
 #library(parallel)
-#options(mc.cores = 2)
-#sequential = {lapply(data$text, remove_stop_words)}
-#multiprocessing = {mclapply(data$text, remove_stop_words)}
-  
+#options(mc.cores = 4)
+#measure_time("parallel::mclapply(data$text, remove_stop_words, nc.cores=4)")
 
-
-measure_time("lapply(data$text, remove_stop_words)")
-
-library(parallel)
-options(mc.cores = 4)
-measure_time("mclapply(data$text, remove_stop_words)")
-
-remove_stop_words("I am all almost ready asshole")
 
 
--- NLP_Multiprocessing_For_Preprocessing_Experiment.py
+++ NLP_Multiprocessing_For_Preprocessing_Experiment.py
@@ -14,7 +14,7 @@ import matplotlib.pyplot as plt
 print(mp.cpu_count())
 
 # read in the data set
-df = pd.read_csv('./Data/news_data.csv', sep=";")
+all_data = pd.read_csv('./Data/news_data_Experimentation.csv', sep=";")
 
 # read in stop word list
 stopwords_list = []
@@ -26,6 +26,9 @@ original_stopwords_list = stopwords_list
 
 
 def remove_stop_words(text):
+    """ A simple function that first splits text via
+    whitespaces and remove then stopwords that appear in list"""
+
     # split text by whitespace
     tokenized_text = str(text).split(" ")
     new_text = ""
@@ -67,53 +70,61 @@ print(len(stopwords_list))
 
 test_dict = {
     # Number of Stop words to remove
-    "#Stopwords": [100, 1000, 2070],
+    "#Stopwords": [100, 2070],
+    # Number of news articles to include
+    "#news_articles": [200, 10000, 500000],
     #libraries and number of cores to test out
-    "Libraries": {"Joblib": [2, 3, 4, 8], "Ray":[2, 3, 4],}
+    "Libraries": {"Joblib": [2, 4, 8], "Ray":[2, 4],}
 }
-for number_of_stopwords in test_dict["#Stopwords"]:
 
-    stopwords_list = original_stopwords_list[:number_of_stopwords]
+for number_of_texts in test_dict["#news_articles"]:
+    # select the desidered number of articles for the experiment
+    df = all_data.head(number_of_texts)
+    for number_of_stopwords in test_dict["#Stopwords"]:
 
-    # to store the results of the experiment
-    model_name_list = []
-    execution_time_list = []
+        stopwords_list = original_stopwords_list[:number_of_stopwords]
+        # to store the results of the experiment
+        model_name_list = []
+        execution_time_list = []
 
-    # sequential methods
-    # from NLP_Preprocessing_Functions import remove_stop_words
-    measure_time("Sequential List", "[remove_stop_words(x) for x in df['text'].to_list()]",
-                 model_name_list, execution_time_list)
-    measure_time("Sequential lambda", "df['text'].apply(lambda x: remove_stop_words(x))",
-                 model_name_list, execution_time_list)
+        # sequential methods
+        # from NLP_Preprocessing_Functions import remove_stop_words
+        measure_time("Sequential List", "[remove_stop_words(x) for x in df['text'].to_list()]",
+                     model_name_list, execution_time_list)
+        measure_time("Sequential lambda", "df['text'].apply(lambda x: remove_stop_words(x))",
+                     model_name_list, execution_time_list)
 
-    # multiprocessing methods
-    for library in test_dict["Libraries"].keys():
+        # multiprocessing methods
+        for library in test_dict["Libraries"].keys():
 
-        for n_core in test_dict["Libraries"][library]:
-            if library == "Joblib":
-                statement = "Parallel(n_jobs=_CORES_)(delayed(remove_stop_words)(i) for i in df['text'].to_list())"
-            else:
-                try:
-                    pool = ray.util.multiprocessing.Pool(processes=nprocs)
-                    statement = "pool.map(remove_stop_words, df['text'].to_list())"
-                except:
-                    pass
-            measure_time("Y X Cores".replace("X",
-                        str(n_core)).replace("Y", library),
-                        statement.replace("_CORES_", str(n_core)), model_name_list, execution_time_list)
+            for n_core in test_dict["Libraries"][library]:
+                if library == "Joblib":
+                    statement = "Parallel(n_jobs=_CORES_)(delayed(remove_stop_words)(i) for i in df['text'].to_list())"
+                else:
+                    try:
+                        pool = ray.util.multiprocessing.Pool(processes=nprocs)
+                        statement = "pool.map(remove_stop_words, df['text'].to_list())"
+                    except:
+                        pass
+                measure_time("Y X Cores".replace("X",
+                            str(n_core)).replace("Y", library),
+                            statement.replace("_CORES_", str(n_core)), model_name_list, execution_time_list)
 
 
-    #plot the resutls
-    plt.figure(figsize=(10,8), dpi=130)
-    plt.style.use("seaborn")
-    plt.bar(model_name_list, execution_time_list)
-    plt.suptitle("Comparison of Multiprocessing Strategies", fontsize=18)
-    plt.title("Number of Stopwords: " + str(number_of_stopwords), fontsize=10)
-    plt.ylabel("Execution Time in Seconds")
-    plt.xlabel("Multiprocessing Approach")
-    plt.axhline(min(execution_time_list), color="red")
-    plt.xticks(rotation=20)
-    plt.savefig(".\data\images\execution_time_plot_" + str(number_of_stopwords) + "_stopwords.png")
+        #plot the resutls
+        plt.figure(figsize=(10,8), dpi=130)
+        plt.style.use("seaborn")
+        plt.bar(model_name_list, execution_time_list)
+        plt.suptitle("Comparison of Multiprocessing Strategies", fontsize=18)
+        plt.title("Number of Texts: " + str(number_of_texts) +
+                  ", Number of Stopwords: " + str(number_of_stopwords), fontsize=10)
+        plt.ylabel("Execution Time in Seconds")
+        plt.xlabel("Multiprocessing Approach")
+        plt.axhline(min(execution_time_list), color="red")
+        plt.xticks(rotation=20)
+        plt.savefig(".\data\images\Multiprocessing_Experiment_Python_" +
+                    str(number_of_texts) + "_articles_" +
+                    str(number_of_stopwords) + "_stopwords.png")
 
 
 
